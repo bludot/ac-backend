39eb72dde8a36c8b77df23e9711e837db95b2112
James <admin@floretos.com>
2022-01-19 18:42:10 +0700

    feat: allow getting state of ac from memory

--- src/modules/ac-api/ac-api.controller.ts
+++ src/modules/ac-api/ac-api.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common'
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common'
 import { ApiOperation } from '@nestjs/swagger'
 import { ACService } from '../ac/ac.service'
 import { ACData } from './ac.dto'
@@ -14,5 +14,14 @@ export class AcApiController {
   })
   async updateAC(@Body() body: ACData) {
     await this.acService.updateAC(body)
+  }
+
+  @Get('/state')
+  @HttpCode(200)
+  @ApiOperation({
+    summary: 'Get AC State',
+  })
+  async getState() {
+    return this.acService.getState()
   }
 }
--- src/modules/ac/ac.service.ts
+++ src/modules/ac/ac.service.ts
@@ -7,13 +7,25 @@ import { IACData } from './interfaces'
 
 @Injectable()
 export class ACService {
+  private adData: IACData
   constructor(private readonly config: ConfigService<ACConfig>) {}
 
   async updateAC(data: IACData): Promise<void> {
     const query: string = queryString.stringify(data)
 
-    axios.post(`${this.config.env.AC_URL}?${query}`).catch((error) => {
+    await axios.post(`${this.config.env.AC_URL}?${query}`).catch((error) => {
       console.error(error)
     })
+    
+    return this.saveState(data)
   }
+
+  saveState(data: IACData): void {
+    this.adData = data
+  }
+
+  getState(): IACData {
+    return this.adData
+  }
+
 }
