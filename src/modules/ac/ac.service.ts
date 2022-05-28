3d26fa4c23cc20c2ec7a43d50f5ea998898264e1
James <admin@floretos.com>
2022-05-28 11:21:36 +0700

    default state

--- src/modules/ac/ac.service.ts
+++ src/modules/ac/ac.service.ts
@@ -8,7 +8,18 @@ import { IACData, IRoomConditions } from "./interfaces";
 @Injectable()
 export class ACService {
   private adData: IACData
-  constructor(private readonly config: ConfigService<ACConfig>) {}
+  constructor(private readonly config: ConfigService<ACConfig>) {
+    this.adData = {
+      "power": 0,
+      "temp": 24,
+      "mode": 0,
+      "fan": 10,
+      "powerful": 0,
+      "quiet": 1,
+      "swingh": 1,
+      "swingv": 1
+    }
+  }
 
   async updateAC(data: IACData): Promise<void> {
     const query: string = queryString.stringify(data)
