8fb36712fdc5f1c9be75096517fd2ff49d142dbe
James Trotter <james@honestbank.com>
2023-01-29 22:30:14 +0700

    feat: mqtt support for homeac

--- src/modules/ac/ac.service.spec.ts
+++ src/modules/ac/ac.service.spec.ts
@@ -5,10 +5,12 @@ import { ACService } from './ac.service'
 import { IACData } from './interfaces'
 import { ACConfig } from './ac.config'
 import { MQTTModule } from '../mqtt/mqtt.module'
+import { MQTTService } from '../mqtt/mqtt.service'
 
 jest.mock('axios')
 describe('ACService', () => {
   let acService: ACService
+  let mqttService: MQTTService
   beforeEach(
     async (): Promise<void> => {
       // tslint:disable-next-line:typedef
@@ -18,10 +20,12 @@ describe('ACService', () => {
       }).compile()
 
       acService = moduleRef.get<ACService>(ACService)
+      mqttService = moduleRef.get<MQTTService>('MQTT')
     },
   )
   describe('updateAC', () => {
     it('should update AC', async () => {
+      mqttService.publish = jest.fn()
       // TODO: Add test for mqtt
       await acService.updateAC(({
         power: 'on',
