dbc080ea000f28b58b3647d953ef09a48dd7b2d8
James Trotter <james@honestbank.com>
2023-01-29 22:01:00 +0700

    feat: add mqtt support

--- src/modules/ac/ac.service.ts
+++ src/modules/ac/ac.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
 import axios from 'axios'
 import * as queryString from 'query-string'
 import { ConfigService } from '../config/config.service'
+import { MQTTService } from '../mqtt/mqtt.service'
 import { ACConfig } from './ac.config'
 import { IACData, IRoomConditions } from "./interfaces";
 
 @Injectable()
 export class ACService {
   private adData: IACData
-  constructor(private readonly config: ConfigService<ACConfig>) {
+  constructor(
+    private readonly config: ConfigService<ACConfig>, 
+    @Inject('MQTT')
+    private readonly mqttService: MQTTService
+    ) {
     this.adData = {
       "power": 0,
       "temp": 24,
@@ -23,10 +28,7 @@ export class ACService {
 
   async updateAC(data: IACData): Promise<void> {
     const query: string = queryString.stringify(data)
-
-    await axios.post(`${this.config.env.AC_URL}/cmd?${query}`).catch((error) => {
-      console.error(error)
-    })
+    this.mqttService.publish('ac', JSON.stringify(data))
 
     return this.saveState(data)
   }
---
+++ src/modules/mqtt/mqtt.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '../config/config.service';
+import { MQTTConfig } from './mqtt.config';
+import * as mqtt from 'mqtt';
+
+@Injectable()
+export class MQTTService {
+  private client: mqtt.MqttClient;
+  constructor(private readonly config: ConfigService<MQTTConfig>) {}
+
+  connect(): void {
+    this.client = mqtt.connect(`mqtt://${this.config.env.MQTT_HOST}:${this.config.env.MQTT_PORT}`, {
+      username: this.config.env.MQTT_USERNAME,
+      password: this.config.env.MQTT_PASSWORD,
+    });
+  }
+
+  publish(topic: string, message: string): void {
+    this.client.publish(topic, message);
+  }
+
+  subscribe(topic: string): void {
+    this.client.subscribe(topic);
+  }
+
+}
